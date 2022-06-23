ee34618f6b726ce0eecfb314bc6785933c59f1d7
Moyosore <31387340+MoyosoreCodes@users.noreply.github.com>
2022-06-23 21:58:31 +0100

    update: disabled-logging

--- config/database.js
+++ config/database.js
@@ -1,17 +1,18 @@
 const {Sequelize} = require('sequelize');
 require('dotenv').config();
 
-const user = "shfhzbutvuxmkz";
-const password = "8811eee095ab45a347b2ef8711d45511d3ae95a4c3208ffffc4d55f83824985c";
-const host = "ec2-44-197-128-108.compute-1.amazonaws.com";
-const port = process.env.PG_PORT;
-const database = "d9s2gurjruonh7";
+// const user = "shfhzbutvuxmkz";
+// const password = "8811eee095ab45a347b2ef8711d45511d3ae95a4c3208ffffc4d55f83824985c";
+// const host = "ec2-44-197-128-108.compute-1.amazonaws.com";
+// const port = process.env.PG_PORT;
+// const database = "d9s2gurjruonh7";
 
 // const isProduction = process.env.NODE_ENV === 'production';
 // const connectionString = ''
 
 const sequelize = new Sequelize("postgres://shfhzbutvuxmkz:8811eee095ab45a347b2ef8711d45511d3ae95a4c3208ffffc4d55f83824985c@ec2-44-197-128-108.compute-1.amazonaws.com:5432/d9s2gurjruonh7", 
     {
+        logging: false,
         dialectOptions: {
             ssl: {
                 require: true,
